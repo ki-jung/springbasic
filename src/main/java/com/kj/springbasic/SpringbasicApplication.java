25b6a882225695d7ef003c421a775437c55131d0
kijung <kijung991008@naver.com>
2024-08-19 17:22:42 +0900

    2024.08.19

--- src/main/java/com/kj/springbasic/SpringbasicApplication.java
+++ src/main/java/com/kj/springbasic/SpringbasicApplication.java
@@ -11,3 +11,8 @@ public class SpringbasicApplication {
 	}
 
 }
+// GET - 데이터를 얻을 때 (RequestBody X)
+// POST - 데이터를 작성할때 (RequestBody O)
+// PUT - 데이터를 전체 수정할 때 (RequestBody O)
+// Patch - 데이터를 일부 수정할 때 (RequestBody O)
+// DELETE - 데이터를 삭제할 때 (RequestBody X)
