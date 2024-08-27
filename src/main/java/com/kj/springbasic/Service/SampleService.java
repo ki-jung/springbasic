bc38f68c7b394a2ed6d45e044b8e286c169f7b56
kijung <kijung991008@naver.com>
2024-08-27 17:14:17 +0900

    2024.08.27

---
+++ src/main/java/com/kj/springbasic/Service/SampleService.java
@@ -0,0 +1,10 @@
+package com.kj.springbasic.Service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.kj.springbasic.dto.PostSample1RequestDto;
+
+public interface SampleService {
+    
+    ResponseEntity<String> postSample1 (PostSample1RequestDto dto);
+}
---
+++ src/main/java/com/kj/springbasic/controller/SampleController.java
@@ -0,0 +1,29 @@
+package com.kj.springbasic.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kj.springbasic.Service.SampleService;
+import com.kj.springbasic.dto.PostSample1RequestDto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sample")
+public class SampleController {
+
+    private final SampleService sampleService;
+
+    @PostMapping("")
+    public ResponseEntity<String> postSample1(
+            @RequestBody @Valid PostSample1RequestDto requestBody) {
+            ResponseEntity<String> response = sampleService.postSample1(requestBody);
+            return response;
+    }
+
+}
